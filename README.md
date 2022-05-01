d773aaf0857d66a4405dd57c7fe08de2687f1fea
Yuki Uchiyama <uchiyama.yuki@moneyforward.co.jp>
2022-05-01 19:23:43 +0900

    Fix code by deno fmt

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ## Overview
+
 Deno sample program for learning.
--- sum_test.ts
+++ sum_test.ts
@@ -1,14 +1,14 @@
-import { assertEquals } from 'https://deno.land/std@0.137.0/testing/asserts.ts';
-import { sum } from './sum.ts';
+import { assertEquals } from "https://deno.land/std@0.137.0/testing/asserts.ts";
+import { sum } from "./sum.ts";
 // deno test
 
-Deno.test('sum() adds given numbers', () => {
+Deno.test("sum() adds given numbers", () => {
   const actual = sum(1, 2);
   const expected = 3;
   assertEquals(actual, expected);
 });
 
-Deno.test('sum() returns 0 when no arguments given', () => {
+Deno.test("sum() returns 0 when no arguments given", () => {
   const actual = sum();
   const expected = 0;
   assertEquals(actual, expected);
